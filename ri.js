e78ce7ce6bddc23595e5d5e4ef06934fc5b8852a
VegazDonghua <ronyoey@gmail.com>
2026-05-11 01:23:23 +0800

    Add files via upload

---
+++ ri.js
@@ -0,0 +1,17 @@
+const playlistRenegadeImmortal = [
+
+{
+title:"Renegade-Immortal Ep 140",
+server1:"PGlmcmFtZSBmcmFtZWJvcmRlcj0iMCIgd2lkdGg9IjY0MCIgaGVpZ2h0PSIzNjAiIHNyYz0iaHR0cHM6Ly9nZW8uZGFpbHltb3Rpb24uY29tL3BsYXllci94aWQwdC5odG1sP3ZpZGVvPWsxZ1F5cjI0VHN2djVuRldOSGEiIGFsbG93ZnVsbHNjcmVlbiBhbGxvdz0iYXV0b3BsYXk7IGZ1bGxzY3JlZW47IHBpY3R1cmUtaW4tcGljdHVyZTsgd2ViLXNoYXJlIj48L2lmcmFtZT4=",
+server2:"",
+server3:""
+},
+
+{
+title:"Renegade-Immortal Ep 139",
+server1:"PGlmcmFtZSB...",
+server2:"https://...",
+server3:""
+}
+
+];
