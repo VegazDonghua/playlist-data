79c551946e8dc7ecb93b4de69598f908c197fdee
VegazDonghua <ronyoey@gmail.com>
2026-05-11 20:46:02 +0800

    Update ri.js

--- ri.js
+++ ri.js
@@ -1,10 +1,6 @@
-const playlistData = [
-
-{
-title:"Renegade-Immortal Ep 140",
-server1:"https://geo.dailymotion.com/player/xid0t.html?video=k1gQyr24Tsvv5nFWNHa",
-server2:"",
-server3:""
-}
-
-];
+[
+  {
+    "title": "Episode 1",
+    "server1": "https://..."
+  }
+]
